92616bb5ebdbf65e32306e0723b00876cff6abbf
Sarp <38987850+sarport@users.noreply.github.com>
2021-10-07 08:40:21 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,9 +5,9 @@ import os, sys
 import cv2
 import numpy as np
 
-print cv2.__version__
+print(cv2.__version__)
 
-print "Hicolor Icon Generator v0.1 by Tomasz Zając (c) 2016\n"
+print("Hicolor Icon Generator v0.1 by Tomasz Zając (c) 2016\n")
 
 iconSrc = sys.argv[1]
 iconRootDir = "//usr//share//icons//hicolor//"
@@ -22,9 +22,9 @@ img = cv2.imread(iconSrc, -1)
 for size in availSizes:
 	width = size / float(np.size(img, 0))
 	height = size / float(np.size(img, 1))
-	print "Resizing to ", size, "x", size, "..."
+	print("Resizing to ", size, "x", size, "...")
 	res = cv2.resize(img, None, fx=width, fy=height, interpolation = cv2.INTER_CUBIC)
-	print "Creating output icon file path..."
+	print("Creating output icon file path...")
 	finalpath = os.path.splitext(iconRootDir + str(size) + "x" + str(size) + "//apps//" + os.path.basename(iconSrc))[0] + '.png'
-	print "Writing icon file to ", finalpath
+	print("Writing icon file to ", finalpath)
 	cv2.imwrite(finalpath, res)
